1164b39d39160b4f698e6d102784040a0b49648d
Pablo Uraga <paburamar@alu.edu.gva.es>
2023-11-10 16:55:51 +0100

    Album html structure

--- album.php
+++ album.php
@@ -1,45 +1,59 @@
-<?php
-include_once(__DIR__ . '/include/head.inc.php');
-include_once(__DIR__ . '/include/connect.inc.php');
-$dbname = 'mysql:host=localhost;dbname=discografia';
-$dbuser = 'vetustamorla';
-$dbpassword = '15151';
-$dboptions = array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8');
-$connection = connect($dbname, $dbuser, $dbpassword, $dboptions);
+<!DOCTYPE html>
+<html lang="en">
 
-$getgrupo = $connection->prepare("SELECT g.nombre, g.codigo FROM grupos g, albumes a WHERE a.codigo = ? AND a.grupo = g.codigo");
-$getgrupo->bindParam(1, $_GET['codigo']);
-$getgrupo->execute();
-$groupinfo = $getgrupo->fetch(PDO::FETCH_ASSOC);
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <?php
+    include_once(__DIR__ . '/include/head.inc.php');
+    include_once(__DIR__ . '/include/connect.inc.php');
+    ?>
+</head>
 
-$getalbuminfo = $connection->prepare("SELECT titulo FROM albumes WHERE codigo = ?");
-$getalbuminfo->bindParam(1, $_GET['codigo']);
-$getalbuminfo->execute();
+<body>
+    <?php
+    $dbname = 'mysql:host=localhost;dbname=discografia';
+    $dbuser = 'vetustamorla';
+    $dbpassword = '15151';
+    $dboptions = array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8');
+    $connection = connect($dbname, $dbuser, $dbpassword, $dboptions);
 
-$getcanciones = $connection->prepare("SELECT titulo, duracion FROM canciones WHERE album = ?");
-$getcanciones->bindParam(1, $_GET['codigo']);
-$getcanciones->execute();
+    $getgrupo = $connection->prepare("SELECT g.nombre, g.codigo FROM grupos g, albumes a WHERE a.codigo = ? AND a.grupo = g.codigo");
+    $getgrupo->bindParam(1, $_GET['codigo']);
+    $getgrupo->execute();
+    $groupinfo = $getgrupo->fetch(PDO::FETCH_ASSOC);
 
+    $getalbuminfo = $connection->prepare("SELECT titulo FROM albumes WHERE codigo = ?");
+    $getalbuminfo->bindParam(1, $_GET['codigo']);
+    $getalbuminfo->execute();
 
+    $getcanciones = $connection->prepare("SELECT titulo, duracion FROM canciones WHERE album = ?");
+    $getcanciones->bindParam(1, $_GET['codigo']);
+    $getcanciones->execute();
 
-echo '<h2>' . $groupinfo['nombre'] . '</h2>';
-echo '<h3>' . $getalbuminfo->fetch(PDO::FETCH_ASSOC)['titulo'] . '</h3>';
+    echo '<h2>' . $groupinfo['nombre'] . '</h2>';
+    echo '<h3>' . $getalbuminfo->fetch(PDO::FETCH_ASSOC)['titulo'] . '</h3>';
 
-echo '<table border=2><tr><td>Titulo</td><td>Duracion</td>';
-while ($canciones = $getcanciones->fetch(PDO::FETCH_ASSOC)) {
-    echo '<tr>';
-    echo '<td>' . $canciones['titulo'] . '</td>';
-    $duracion = $canciones['duracion'];
+    echo '<table border=2><tr><td>Titulo</td><td>Duracion</td>';
+    while ($canciones = $getcanciones->fetch(PDO::FETCH_ASSOC)) {
+        echo '<tr>';
+        echo '<td>' . $canciones['titulo'] . '</td>';
+        $duracion = $canciones['duracion'];
 
-    $minutes = floor($duracion/60);
-    $seconds = $duracion%60;
+        $minutes = floor($duracion / 60);
+        $seconds = $duracion % 60;
 
-    if ($seconds < 10) {
-        $seconds = '0' . $seconds;
+        if ($seconds < 10) {
+            $seconds = '0' . $seconds;
+        }
+
+        echo '<td>' . $minutes . ':' . $seconds . '</td>';
     }
+    echo '</table>';
 
-    echo '<td>' . $minutes . ':' . $seconds . '</td>';
-}
-echo '</table>';
+    echo '<a href="/group/' . $groupinfo['codigo'] . '">Volver</a>';
+    ?>
+</body>
 
-echo '<a href="/group/' . $groupinfo['codigo'] . '">Volver</a>';
+</html>
