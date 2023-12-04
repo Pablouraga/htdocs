5136eb6d57bae9de3dfa0bf92e7eeec260073be4
Pablo Uraga <paburamar@alu.edu.gva.es>
2023-12-04 15:34:57 +0100

    json

---
+++ All/jsonPabloUraga.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * @author Pablo Uraga Martinez
+ * @version 1.0.0
+ * 
+ * get products from merchashop database, return as json
+ */
+    $dsn = 'mysql:dbname=merchashop;host=127.0.0.1';
+    $user = 'Lumos';
+    $pass = 'Nox';
+    $connection = new PDO($dsn, $user, $pass);
+    if (empty($_GET['product'])) {
+        $query = $connection->query('SELECT * FROM products');
+    } else {
+        $query = $connection->prepare('SELECT name, category, price, sale, stock, image FROM products WHERE id=?');
+        $query->execute([$_GET['product']]);
+    }
+    $query = $query->fetchAll(PDO::FETCH_ASSOC);
+    header('Content-Type: application/json; charset=utf-8');
+    echo json_encode($query);
